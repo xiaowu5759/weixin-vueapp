0267f0ced790df2b065acde046fa23f6168e3bdc
XiaoWu-5759 <1981293730@qq.com>
2019-08-28 14:11:25 +0800

    header组件成型

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -14,34 +14,136 @@
                     {{seller.description}}/{{seller.deliveryTime}}分钟送达
                 </div>
                 <div v-if="seller.supports" class="support">
-                    <span class="icon"></span>
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
+            <div v-if="seller.supports" class="support-count">
+                <span class="count">{{seller.supports.length}}个</span>
+                <!-- 箭头 小图标 -->
+                <i class="icon-keyboard_arrow_right"></i>
+            </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper">
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
-    props: ["seller"]
+    props: ["seller"],
+    created() {
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    }
 };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixin";
     .header
+        position: relative
         color: #fff
-        background: #000
+        background: #999
         .content-wrapper
+            position: relative
             padding: 24px 12px 18px 24px
             font-size: 0
             .avatar
                 display: inline-block
+                vertical-align: top
+                img
+                    border-radius: 2px
             .content
                 display: inline-block
-                font-size: 14px
+                // font-size: 14px
                 margin-left: 16px
                 .title
                     margin: 2px 0 8px 0
+                    .brand
+                        display : inline-block
+                        vertical-align : top
+                        width : 30px
+                        height : 18px
+                        bg-image('brand')
+                        background-size : 30px 18px
+                        background-repeat : no-repeat
+                    .name
+                        margin-left : 6px
+                        font-size : 16px
+                        line-height : 18px
+                        font-weight : bold
+                .description
+                    margin-bottom : 10px
+                    line-height : 12px
+                    font-size: 12px
+                .support
+                    .icon
+                        display : inline-block
+                        vertical-align : top
+                        width : 12px
+                        height : 12px
+                        margin-right : 4px
+                        background-size : 12px 12px
+                        background-repeat : no-repeat
+                        &.decrease
+                            bg-image('decrease_1')
+                        &.discount
+                            bg-image('discount_1')
+                        &.guarantee
+                            bg-image('guarantee_1')
+                        &.invoice
+                            bg-image('invoice_1')
+                        &.special
+                            bg-image('special_1')
+                    .text
+                        line-height : 12px
+                        font-size: 10px
+            .support-count
+                position: absolute
+                right : 12px
+                bottom : 14px
+                padding : 0 8px
+                height : 24px
+                line-height : 24px
+                border-radius : 14px
+                background : rgba(0,0,0,0.2)
+                text-align : center
+                .count
+                    vertical-align: top
+                    font-size: 10px
+                .icon-keyboard_arrow_right
+                    margin-left : 2px
+                    line-height : 24px
+                    font-size: 10px
+        .bulletin-wrapper
+            position : relative
+            height : 28px
+            line-height : 28px
+            padding: 0 22px 0 12px
+            white-space : nowrap
+            overflow: hidden
+            text-overflow : ellipsis
+            background : rgba(7, 17, 27, 0.2)
+            // font-size: 0
+            .bulletin-title
+                display : inline-block
+                vertical-align : top
+                margin-top : 7px
+                width : 22px
+                height : 12px
+                bg-image('bulletin')
+                background-size : 22px 12px
+                background-repeat: no-repeat
+            .bulletin-text
+                vertical-align : top
+                margin: 0 4px
+                font-size: 10px
+            .icon-keyboard_arrow_right
+                // 脱离文档流，绝对定位
+                position : absolute
+                right : 12px
+                top : 8px
+                font-size : 10px
 </style>
