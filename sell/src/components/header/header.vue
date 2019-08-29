74de6b1d4d47f6a631fd05072d3be53588ecf16b
XiaoWu-5759 <1981293730@qq.com>
2019-08-29 17:39:45 +0800

    蒙层完善

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -44,6 +44,21 @@
                         <div class="text">优惠信息</div>
                         <div class="line"></div>
                     </div>
+                    <ul v-if="seller.supports" class="supports">
+                        <li class="support-item" v-for="(item,index) in seller.supports" :key="index">
+                            <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                            <span class="text">{{seller.supports[index].description}}</span>
+                        </li>
+                    </ul>
+                    <!-- todo 自适应的线 可以封装成组件 -->
+                    <div class="title">
+                        <div class="line"></div>
+                        <div class="text">商家公告</div>
+                        <div class="line"></div>
+                    </div>
+                    <div class="bulletin">
+                        <p class="content">{{seller.bulletin}}</p>
+                    </div>
                 </div>
             </div>
             <div class="detail-close">
@@ -226,7 +241,7 @@ export default {
                     .title
                         display : flex
                         width : 80%
-                        margin: 30px auto 24px
+                        margin: 28px auto 24px
                         .line
                             flex : 1
                             position : relative
@@ -234,7 +249,46 @@ export default {
                             border-bottom : 1px solid rgba(255, 255, 255, 0.2)
                         .text
                             padding : 0 12px
+                            font-weight : 700
                             font-size : 14px
+                    .supports
+                        width : 80%
+                        margin: 0 auto
+                        .support-item
+                            padding: 0 12px
+                            margin-bottom : 12px
+                            font-size : 0
+                            &:last-child
+                                margin-bottom : 0
+                            .icon
+                                display : inline-block
+                                width : 16px
+                                height : 16px
+                                vertical-align : top
+                                margin-right : 6px
+                                background-size : 16px 16px
+                                background-repeat: no-repeat
+                                &.decrease
+                                    bg-image('decrease_2')
+                                &.discount
+                                    bg-image('discount_2')
+                                &.guarantee
+                                    bg-image('guarantee_2')
+                                &.invoice
+                                    bg-image('invoice_2')
+                                &.special
+                                    bg-image('special_2')
+                            .text
+                                font-size : 12px
+                                line-height : 16px
+                                color : rgb(255, 255, 255)
+                    .bulletin
+                        width : 80%
+                        margin : 24px auto
+                        .content
+                            padding : 0 12px
+                            line-height : 24px
+                            font-size: 12px
             .detail-close
                 position : relative
                 width : 32px
