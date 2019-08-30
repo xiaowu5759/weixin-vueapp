0d3d2b6589ad5dcf6dbfc01168d66ad53c05bc68
XiaoWu-5759 <1981293730@qq.com>
2019-08-30 15:46:09 +0800

    betterscroll的使用

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -2,9 +2,7 @@
   <div>
     <!-- <img src="./assets/logo.png">
     <router-view/>-->
-    <div class="header">
-      <v-header :seller="seller"></v-header>
-    </div>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -55,19 +53,19 @@ export default {
 
 <style lang="stylus" rel="stylesheet/stylus">
  @import "./common/stylus/mixin.styl"
-.tab
-  display: flex
-  width: 100%
-  height: 40px
-  line-height: 40px
-  border-1px(rgba(7, 17, 27, 0.1))
-  .tab-item
-    flex: 1
-    text-align: center
-    & > a
-      display: block
-      font-size: 14px
-      color: rgb(77, 85, 93)
-      &.active
-        color: rgb(240, 20, 20)
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
 </style>
